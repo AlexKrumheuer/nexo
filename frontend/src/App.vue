2ffcca7fc78aab5d8ce2f7e590e3507c7602b2e0
AlexKrumheuer <alexkrumheuerbr@gmail.com>
2026-01-10 16:30:41 -0300

    correção de divergências de versão

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import Login from './components/auth/Login.vue';
+</script>
+
+<template>
+  <router-view>
+
+  </router-view>
+</template>
+
+<style>
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    width: 100vw;
+    height: 100vh;
+    font-family: "Inter", "Arial";
+    font-size: 62.5%;
+  }
+
+  #app {
+    width: 100%;
+    height: 100%;
+
+  }
+</style>
