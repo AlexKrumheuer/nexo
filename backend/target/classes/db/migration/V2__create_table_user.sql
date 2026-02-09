f6bc43a210afd2ee7b1563aacbce811a98859e5d
AlexKrumheuer <alexkrumheuerbr@gmail.com>
2026-02-08 21:18:51 -0300

    Chore: removendo pasta target do git

--- backend/target/classes/db/migration/V2__create_table_user.sql
+++
@@ -1,7 +0,0 @@
-create table users(
-    id bigint primary key auto_increment,
-    username varchar(500) not null,
-    email varchar(500) not null unique,
-    password varchar(500) not null,
-    role varchar(20)
-);
--- backend/target/classes/db/migration/V4__create_token_blacklist.sql
+++
@@ -1,5 +0,0 @@
-create table token_blacklist(
-    id bigint primary key auto_increment,
-    token varchar(500) not null unique,
-    expiration_date TIMESTAMP not null
-);
